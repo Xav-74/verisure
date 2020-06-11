bb89f4fc1d4a0a6ef6164193e2e9a843e4b4f167
Xav-74 <56156622+Xav-74@users.noreply.github.com>
2020-06-11 11:19:54 +0200

    Add update cmd

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,14 +19,26 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function verisure_install() {
+
+	message::add('verisure', 'Merci pour l\'installation du plugin Verisure. Lisez bien la documentation avant utilisation et n\'hésitez pas à laisser un avis sur le Market Jeedom !');
 	
 }
 
 function verisure_update() {
 
+	// Mise à jour de l'ensemble des commandes pour chaque équipement
+    log::add('verisure', 'info', 'Mise à jour en cours des commandes du Plugin Verisure');
+    foreach (eqLogic::byType('verisure') as $eqLogic) {
+        $eqLogic->save();
+        log::add('verisure', 'info', 'Mise à jour des commandes effectuée pour l\'équipement '. $eqLogic->getHumanName());
+    }
+	message::add('verisure', 'Merci pour la mise à jour du plugin Verisure. Consultez les notes de version ( https://xav-74.github.io/verisure/fr_FR/changelog ) avant utilisation et n\'hésitez pas à laisser un avis sur le Market Jeedom !');
+	
  }
 
 function verisure_remove() {
+
+	message::add('verisure', 'Le plugin Verisure a été correctement désinstallé. N\'hésitez pas à laisser un avis sur le Market Jeedom !');
 
 }
 
